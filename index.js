a56287401861fcb6134654ab7d7aec9deb8a6cac
Atul Sharma <105098931+Atulsharma2004@users.noreply.github.com>
2023-08-22 01:37:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ app.post('/checkout-payment', async (req, res) => {
 })
 
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 
